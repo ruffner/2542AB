66529cca57891d39e16d2410060199ba6057ff65
Matt Ruffner <mattruffner7@gmail.com>
2014-12-27 21:38:03 -0500

    Added comments Deleted ```int ld``` from debugging Added getter and setter for decimal point Capped brightness at 40

--- D2542.cpp
+++ D2542.cpp
@@ -11,10 +11,13 @@
      D
 */
 
-D2542*  D2542::isr_handler;
+// static member definitions
+D2542*  D2542::handler;
 
+// the refresh ISR
 ISR(TIMER1_COMPA_vect)
 {
+  // single out our singleton
   D2542::isr();
 }
 
@@ -27,8 +30,13 @@ D2542::D2542()
 
 }
 
+// dont fear it, it doesnt bite
+//               A            B           C           D
 D2542::D2542(int16_t Ap, int16_t Bp, int16_t Cp, int16_t Dp,
+	     //   E         F           G       decimal point
              int16_t Ep, int16_t Fp, int16_t Gp, int16_t pP,
+	     // numbering starts with the half digit and moves left
+	     // ground1   ground2     ground3      ground4
              int16_t g1, int16_t g2, int16_t g3, int16_t g4)
 {
   int i;
@@ -46,24 +54,30 @@ D2542::D2542(int16_t Ap, int16_t Bp, int16_t Cp, int16_t Dp,
   grounds[3] = g4;
   pPin = pP;
   
+  // segments outputs
   for(i = 0; i < 7; i++)
     pinMode(segments[i], OUTPUT);
+  // grounds outputs
   for(i = 0; i < 4; i++)
     pinMode(grounds[i], OUTPUT);
   pinMode(pPin, OUTPUT);
   
+  // off now
   for(i = 0; i < 4; i++)
     digitalWrite(grounds[i], HIGH);
   
-  dp = true;
+  dp = false;
   curNum = 0;
   isDispOn = false;
   max = 1999;
   min = 0;
-  isr_handler = this;
+  // dress the singleton
+  handler = this;
+  // setup refresh
   setupTimer1();
 }
 
+// tp display
 void D2542::setNumber(int num)
 {
   if(num < min) num = min;
@@ -76,6 +90,7 @@ int D2542::getNumber()
   return curNum;
 }
 
+// enable refresh and update members
 void D2542::on()
 {
   // enable the interrupt that cycles the updates
@@ -84,7 +99,7 @@ void D2542::on()
   isDispOn = true;
 }
 
-
+// disable interrupts
 void D2542::off()
 {
   // stop interrupting us
@@ -118,7 +133,8 @@ void D2542::setMin(int nMin)
 
 void D2542::setBrightness(int b)
 {
-  if(b > 100) b = 100;
+  if(b > 50) b = 50;
+  if(b < 1) b = 1;
   brightness = b;
 }
 
@@ -127,10 +143,20 @@ int D2542::getBrightness()
   return brightness;
 }
 
+void D2542::setDP(bool d)
+{
+  dp = d;
+}
+
+bool D2542::getDP()
+{
+  return dp;
+}
+
 void D2542::isr()
 {
-  if(isr_handler)
-    isr_handler->update();
+  if(handler)
+    handler->update();
 }
 
 //---
@@ -140,6 +166,7 @@ void D2542::isr()
 // setup the interrupt timer to update the display
 void D2542::setupTimer1()
 {
+  // CTC mode with clk/64 prescale, 4ms/tick
   TCCR1B |= (1 << WGM12) | (1 << CS11) | (1 << CS10);
   // 32 ms period, 31.25 updates/sec
   OCR1AL = 8;
@@ -149,26 +176,34 @@ void D2542::update()
 {
   int i, j;
 
+  // TODO: spend time optimizing this shitstorm
+  //       of determining digits
   uint8_t digits[4];
   digits[0] = floor(curNum/1000.0);
   digits[1] = floor(((curNum - (digits[0]*1000))/100));
   digits[2] = floor((curNum % 100)/10);
   digits[3] = floor((curNum % 10));
 
-  ld = digits[3];
-
+  // iterate through digits
   for(i = 0; i < 4; i++) {
+    // iterate through segments
     for(j = 0; j < 7; j++) {
+      // if on th first digit, just do the B and C segments
       if(!i) {
         digitalWrite(segments[1], digits[0]);
         digitalWrite(segments[2], digits[0]);
+	// were done for this digit
         break;
       }
+      // decimal is on third anode
       if(i == 2)
 	digitalWrite(pPin, dp);
-
+      
+      // assign the segments to the value in the matrix
       digitalWrite(segments[j], CHARS[digits[i]][j]);
     }
+    
+    // the 'brightness' control, just turn it on for a bit.
     digitalWrite(grounds[i], LOW); 
     for(j=0; j < brightness; j++) __asm__("nop\n\t");
     digitalWrite(grounds[i], HIGH);
--- D2542.h
+++ D2542.h
@@ -4,7 +4,7 @@
 #include "Arduino.h"
 
 // TODO: store digit codes in a singe byte
-//       and ditch this quick fix
+//       and ditch this quick fix. 
 static const bool CHARS[10][7] = {{1,1,1,1,1,1,0},
                               {0,1,1,0,0,0,0},
                               {1,1,0,1,1,0,1},
@@ -20,48 +20,77 @@ class D2542 {
  public:
   
   D2542();
+  //      A            B           C           D
   D2542(int16_t Ap, int16_t Bp, int16_t Cp, int16_t Dp,
+	//   E         F           G       decimal point
         int16_t Ep, int16_t Fp, int16_t Gp, int16_t pP,
+	// numbering starts with the half digit and moves left
+	// ground1   ground2     ground3      ground4
         int16_t g1, int16_t g2, int16_t g3, int16_t g4);
 
+  // getters and setters for changeable settings
+
+  // the number to show, 0-1999
   void setNumber(int num);
   int getNumber();
-
+  
+  // easy way to put a limit on what is showing
   void setMax(int m);
   int getMax();
   void setMin(int m);
   int getMin();
 
-  void setBrightness(int brightness);
+  // decimal point
+  void setDP(bool d);
+  bool getDP();
+
+
+  // this just controls how many NOPs each digit 
+  // is on as it scans to update. the number correlates 
+  // directly to the NOP count, past 40  or so (~2.5ms @ 16Mhz)
+  // the difference becomes less noticeable
+  void setBrightness(int b);
   int getBrightness();
   
+  // enables the timer1 overflow interrupt that
+  // controls refreshing, default is about 31/sec
   void on();
+
+  // disables the interrupt and darkens the display
   void off();
+  // getter
   bool isOn();
   
+  // the real ISR's shoe into the members
   static void isr();
 
-  int ld;
-
  protected:
+  // the meat of the ISR routine
   void update();
 
  private:
+  // configure the update routine
+  void setupTimer1();
+
+  // pin containers
   int16_t segments[7];
   int16_t pPin;
   int16_t grounds[4];
 
+  // the caps
   int max;
   int min;
+  // 0 will just be very dim, past 40 its just wasteful
   int brightness;
 
-  static  D2542* isr_handler;
+  // the reference for the ISR to use
+  static  D2542* handler;
 
   int curNum;
   bool isDispOn;
-  bool dp;
 
-  void setupTimer1();
+  // decimal control
+  bool dp;
 
 };
 
